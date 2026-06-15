1cf60371e642564cd21e05ac921b68f4b1d00672
shabanhr <sshahaider@gmail.com>
2026-06-15 12:47:09 +0500

    feat(lyrics-card): enable prefetching for lyrics link

--- components/lyrics-card/index.tsx
+++ components/lyrics-card/index.tsx
@@ -20,6 +20,7 @@ export const LyricsCard = ({ data }: Props) => {
 		<Link
 			href={getLyricsURL(slug)}
 			className="hover:bg-accent/80 active:bg-accent hover:text-accent-foreground relative flex flex-col rounded-lg p-2 duration-100"
+			prefetch={true}
 		>
 			<AnimatedImage alt={title} src={getImageURL({ slug, oldSlug })} />
 			<div className="space-y-1 px-1 pt-2 pb-1">
